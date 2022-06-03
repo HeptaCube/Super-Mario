dcc50b0866e65b878674a8ccca0dc6a11b89d021
Denim <105928841+Floppyworld@users.noreply.github.com>
2022-06-03 17:50:10 +0900

    WIP Version 1

---
+++ Assets/Scripts/AnimatedSprite.cs
@@ -0,0 +1,101 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class AnimatedSprite : MonoBehaviour
+{
+    // 요소들을 담을 변수.
+    public SpriteRenderer getSpriteRenderer { get; private set; }
+    public PlayerController getPlayerController { get; private set; }
+
+    public Sprite[] sprites;
+    public float animationTime = 0.25f;
+    public int animationFrame { get; private set; }
+    public bool loop = true;
+
+    // idle, jump 관련 변수들.
+    public Sprite idleSprite;
+    public Sprite jumpSprite;
+    public bool isIdle;
+    public bool isJump;
+
+
+    private void Awake()
+    {
+        this.getSpriteRenderer = GetComponent<SpriteRenderer>();
+        this.getPlayerController = GetComponent<PlayerController>();
+        isIdle = true;
+    }
+
+
+    private void Start()
+    {
+        // 애니메이션이 돌아가는 메소드의 반복을 시작한다.
+        InvokeRepeating(nameof(Advance), this.animationTime / 5, this.animationTime / 5);
+    }
+
+
+    // 스프라이트의 상태를 결정한다.
+    private void SetSpriteStatus()
+    {
+        // isJump가 참이라면 스프라이트를 점프 상태로 바꾼다.
+        if (isJump)
+        {
+            this.getSpriteRenderer.sprite = jumpSprite;
+            this.animationFrame = 0;
+            return;
+        }
+        // 그렇지 않다면 idle도 검사한다.
+        else if (isIdle)
+        {
+            this.getSpriteRenderer.sprite = idleSprite;
+            this.animationFrame = 0;
+            return;
+        }
+    }
+
+
+    private void Advance()
+    {
+        this.animationFrame++;
+
+        // "애니메이션 프레임이 최대길이보다 같거나 많은" 상태이고 "loop가 true" 라면 
+        if (this.animationFrame >= this.sprites.Length && this.loop && !isIdle)
+        {
+            // 애니메이션의 프레임을 0으로 되돌림.
+            this.animationFrame = 0;
+        }
+
+        // "애니메이션 프레임이 0 이상인 상태"이고 "애니메이션 프레임이 스프라이트 길이 미만"인 경우
+        // 스프라이트가 동작한다.
+        if (this.animationFrame >= 0 && this.animationFrame < this.sprites.Length && !isIdle && !isJump)
+        {
+            this.getSpriteRenderer.sprite = this.sprites[this.animationFrame];
+        }
+    }
+
+
+    // 스프라이트의 방향을 결정해줌.
+    private void spriteDirection()
+    {
+        gameObject.transform.localScale = new Vector2(this.getPlayerController.spriteDirection, 1);
+    }
+
+
+    // 필요한 메소드들을 매 프레임마다 불러와줌.
+    private void Update()
+    {
+        spriteDirection();
+        SetSpriteStatus();
+    }
+
+
+    public void Restart()
+    {
+        // 이 값을 0으로 하면 Advance() 메서드에서 this.animationFrame++ 가 적용되므로
+        // 0이 아니라 1부터 시작하게 된다. 그렇기 때문에 -1로 초기화한다.
+        this.animationFrame = -1;
+        Advance();
+    }
+}
---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,157 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    // 외부 스크립트에서 접근할 수 없지만 [SerializeField]를 통해
+    // private 변수를 인스펙터 창에서 접근할 수 있게 해줌.
+    // 오브젝트, 캐릭터마다 다른 수치이므로 인스펙터에서 조절이 가능해야 함.
+
+    [SerializeField]
+    float jumpPower = 8f;
+
+    [SerializeField]
+    float moveSpeed = 5f;
+
+    [SerializeField]
+    float minJumpHeight = 1f;
+
+    [SerializeField]
+    float jumpCutDelay = 0.5f;
+
+    [SerializeField]
+    float maxJumpTime = 0.1f;
+
+    // 필요한 요소들을 불러올 변수.
+    Collider2D getCollider2D;
+    Rigidbody2D getRigidbody2D;
+    PlayerController getPlayerController;
+
+    // transform.position.y 값을 담아줄 변수.
+    float endJumpHeight = 0f;
+
+    // 점프 거리와 관련된 변수.
+    float startJumpHeight = 0f;
+    float jumpDistance = 0f;
+
+    // [플레이어] 버튼 인식과 관련된 변수.
+    int recognizedKeyInput;
+
+
+    private void Start()
+    {
+        // Collider2D와 Rigidbody2D를 불러옴.
+        getCollider2D = GetComponent<Collider2D>();
+        getRigidbody2D = GetComponent<Rigidbody2D>();
+
+        // [플레이어] 버튼을 인식함.
+        getPlayerController = GetComponent<PlayerController>();
+    }
+
+
+
+    // 필요한 메소드들을 매 프레임마다 불러와줌.
+    private void Update()
+    {
+        OnJump();
+        OnMove();
+        // recognizedKeyInput 을 갱신해줌.
+        recognizedKeyInput = this.getPlayerController.recognizedKey;
+    }
+
+
+    void OnJump()
+    {
+        Vector2 jumpVelocity = this.getRigidbody2D.velocity;
+        float clampedJumpVertical = Mathf.Clamp(jumpVelocity.y, -0.01f, 0f);
+        bool isRangedJumpVertical = clampedJumpVertical == jumpVelocity.y;
+
+
+        // 점프 거리가 점프최대거리보다 많다면 리턴함. (점프 최대거리 제한)
+        if (isRangedJumpVertical && this.recognizedKeyInput == 1)
+        {
+            // PlayerController.cs의 점프 사운드를 재생하는 메소드를 실행함.
+            getPlayerController.SendMessage("PlayAudio");
+
+            startJumpHeight = transform.position.y;
+            getRigidbody2D.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+        }
+
+
+        if (this.recognizedKeyInput == 2)
+        {
+            // 버튼을 뗀 시점에 y 포지션 값을 저장함.
+            endJumpHeight = transform.position.y;
+            // 점프 시작 시점부터 끝난 시점가지의 거리를 구함.
+            jumpDistance = endJumpHeight - startJumpHeight;
+
+            // 떨어지는 시점에 점프할수 없게 만들어줌.
+            if (jumpVelocity.y < 0)
+            {
+                return;
+            }
+
+
+            // 점프 거리가 최소 점프 높이보다 낮다면 제한된 점프를 시작함.
+            if (jumpDistance < minJumpHeight)
+            {
+                print("최소 점프컷에 걸림.");
+                InvokeStopJump(jumpCutDelay);
+            }
+            // 어느정도(예: 2.5초) 길게 점프키를 눌렀다면 무조건 끝까지 점프하도록 함.
+            else if (jumpDistance > maxJumpTime)
+            {
+                print("최대 점프컷에 걸림.");
+                return;
+            }
+            // 그렇지 않다면 일반 점프를 시작함.
+            else
+            {
+                print("일반 점프.");
+                StopJump();
+            }
+        }
+    }
+
+
+    // 제한된 점프를 구현하는 코루틴 메소드.
+    void InvokeStopJump(float sec)
+    {
+        StartCoroutine(StopJump(sec));
+    }
+
+    // 점프를 멈추는 코루틴 메소드.
+    IEnumerator StopJump(float sec)
+    {
+        yield return new WaitForSeconds(sec);
+        StopJump();
+    }
+
+    // 점프를 멈추는 메소드.
+    void StopJump()
+    {
+        getRigidbody2D.velocity = new Vector2(1 * Input.GetAxis("MoveX") * moveSpeed, 1);
+
+        // 혹시 몰라 남겨둔 코드.
+        // getRigidbody2D.AddForce(Vector2.down * jumpPower);
+        // getRigidbody2D.AddForce(Vector2.down * jumpPower * 0.3f, ForceMode2D.Impulse);
+    }
+
+
+    // 좌우 방향을 반환해주는 메소드.
+    Vector2 MoveDirection
+    {
+        get
+        {
+            // Vector2.right에 인풋값에 따라 양수 또는 음수를 곱함.
+            return (Vector2.right * Input.GetAxis("MoveX"));
+        }
+    }
+
+    void OnMove()
+    {
+        // 반환받은 MoveDirection Vector2 메소드의 값으로 좌우로 움직이게 해주는 코드.
+        getRigidbody2D.velocity = new Vector2(MoveDirection.x * moveSpeed, getRigidbody2D.velocity.y);
+    }
+}
